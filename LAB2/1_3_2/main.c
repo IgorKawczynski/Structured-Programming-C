6c0fae3215084c494673ae0f04602a20cf846e51
Igor <kluseczkibabuni94@gmail.com>
2021-02-22 15:58:41 +0100

    modyfing second lab

---
+++ LAB2/1_3_2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x;
+    int y;
+    scanf("%d\n%d", &x, &y);
+    if(x>y)
+    printf("%d", x);
+    else
+        printf("%d", y);
+    return 0;
+}
