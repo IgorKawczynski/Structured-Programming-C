6542b59376db4492be76bbc935114b805ca831c1
Igor <kluseczkibabuni94@gmail.com>
2021-03-29 12:50:38 +0200

    Modifying Utrwalenie

--- Utrwalenie/WARIANT_A_2/main.c
+++ Utrwalenie/WARIANT_A_2/main.c
@@ -7,9 +7,9 @@ int rekursja(unsigned int n){
 
     if(n==0)return 2;
     if(n==1)return 2;
-    if(n%2!=0)
-    if(n%2==0)
-
+    if(n%2==0)return rekursja(n/2-1)+n/2;
+    if(n%2!=0)return 2*rekursja(n-1)-(n-1)/2;
+}
 
 
 int main()
