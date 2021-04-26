e46e073824ddb221bb672f30b2447d5d1f22c094
Igor <kluseczkibabuni94@gmail.com>
2021-04-26 19:41:49 +0200

    Adding few more tasks to LAB4

---
+++ LAB4/3_2_16/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void przepis(const int *x, int *const y){
+
+    *y=*x;
+
+
+}
+
+
+int main()
+{
+    int x;
+    printf("Podaj x : \n");
+    scanf("%d", &x);
+    int y;
+    printf("Podaj y : \n");
+    scanf("%d", &y);
+    printf("Przed przepisaniem x=%d  ,  y=%d\n", x, y);
+    przepis(&x, &y);
+    printf("PO przepisaniu x=%d  ,  y=%d\n", x, y);
+    return 0;
+}
--- LAB4/3_2_2/main.c
+++ LAB4/3_2_2/main.c
@@ -4,7 +4,7 @@
 int *min(int *a, int *b){
 if(*a<*b)return a;
 
-return b; //teraz zwracamy adres, nie wskaznik, nie wartosc
+return b; //teraz zwracamy wskaznik ( adres ), nie wartosc
 }
 
 
@@ -12,7 +12,7 @@ return b; //teraz zwracamy adres, nie wskaznik, nie wartosc
 int main()
 {
     int x=44;
-    int y=7;
+    int y=56;
 
     printf("%d --- %p\n", x, &x);
     printf("%d --- %p\n", y, &y);
