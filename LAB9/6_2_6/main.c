7c2b01bc153bfbcbd995902e6e67b98ee80fed9b
Igor <kluseczkibabuni94@gmail.com>
2021-05-18 20:28:52 +0200

    Modifying LAB9

--- LAB9/6_2_6/main.c
+++ LAB9/6_2_6/main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* W zadaniu 6_2_6 : Nie bardzo wiem jak zwolniæ ca³¹ 3wymiarow¹ tablicê w tej wersji - przy wersji wskaŸnikowej w zadaniu 6_2_5
- zwalniam ka¿dy wymiar po kolei i program dzia³a prawid³owo, ale w tym wypadku, gdy próbujê zrobiæ to samo, program na koniec nie zwraca zera, wiêc domyœlam siê, ¿e coœ robiê Ÿle.
-Próbuj¹c jednak zwolniæ tablicê tylko poprzez ,, free(tab) '' - funkcja zwalnia tylko pierwszy wymiar, a reszta pozostaje bez zmian, chyba ¿e coœ Ÿle rozumujê, aczkolwiek wartoœci oprócz pierwszego wiersza pozostaj¹ takie same jak wczeœniej wprowadzone
-W zadaniu 6_2_14 : Program nie bardzo chce dzia³aæ i wynika to chyba ze Ÿle zdeklarowanej tablicy, tablice 3wymiarowe deklarowane w ten sposób zdecydowanie s¹ dla mnie mniej zrozumia³e od wskaŸnikowej wersji
- */
+
 int (**foo(unsigned int n, unsigned int m, unsigned int k))[]{
 
     return malloc(n*sizeof(int[m][k]));
