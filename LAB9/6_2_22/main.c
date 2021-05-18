3db24fff5eb49daf693c3c9c44e803454e9e40c2
Igor <kluseczkibabuni94@gmail.com>
2021-05-18 20:26:12 +0200

    Modifying LAB9

--- LAB9/6_2_22/main.c
+++ LAB9/6_2_22/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* W zadaniu 6_2_6 : Nie bardzo wiem jak zwolniæ ca³¹ 3wymiarow¹ tablicê w tej wersji - przy wersji wskaŸnikowej w zadaniu 6_2_5
- zwalniam ka¿dy wymiar po kolei i program dzia³a prawid³owo, ale w tym wypadku, gdy próbujê zrobiæ to samo, program na koniec nie zwraca zera, wiêc domyœlam siê, ¿e coœ robiê Ÿle.
-Próbuj¹c jednak zwolniæ tablicê tylko poprzez ,, free(tab) '' - funkcja zwalnia tylko pierwszy wymiar, a reszta pozostaje bez zmian, chyba ¿e coœ Ÿle rozumujê, aczkolwiek wartoœci oprócz pierwszego wiersza pozostaj¹ takie same jak wczeœniej wprowadzone
-*/
-
 void elementy(int n, int m, int tab[][m]){
 
     for(int i=0; i<n; i++)
